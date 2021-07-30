66d0099e9b4a66b8f982c692400c76937088aea3
JohanRoyer <49339419+JohanRoyer@users.noreply.github.com>
2021-07-30 14:09:04 +0200

    Update Onboarding_Sol.py

--- Onboarding_Sol.py
+++ Onboarding_Sol.py
@@ -1,27 +1,15 @@
-import sys
-import math
-
-# The while loop represents the game.
-# Each iteration represents a turn of the game
-# where you are given inputs (the heights of the mountains)
-# and where you have to print an output (the index of the mountain to fire on)
-# The inputs you are given are automatically updated according to your last actions.
-
-
 # game loop
 while 1:
-    highest_mountain = 0  #initialisation size of the highest mountain
-    target_mountain = 0    #number of the mountain that will be shot
-    for i in range(8):
-        mountain_h = int(input())  # represents the height of one mountain.
-        if highest_mountain < mountain_h:
-            highest_mountain = mountain_h
-            target_mountain = i
-    
+    enemy_1 = input()  # name of enemy 1
+    dist_1 = int(input())  # distance to enemy 1
+    enemy_2 = input()  # name of enemy 2
+    dist_2 = int(input())  # distance to enemy 2
 
     # Write an action using print
-    # To debug: print("Debug messages...", file=sys.stderr, flush=True)
 
-    # The index of the mountain to fire on.
+    # Enter the code here
 
-    print(target_mountain)   
+    if dist_1 < dist_2:
+        print(enemy_1)
+    else:
+        print(enemy_2)
